eb3fcfb0463cc3695c9fc01addf3f34f17cb0417
machine <137556120+machine-del@users.noreply.github.com>
2024-07-14 17:44:09 +0500

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="header-box">
+                <router-link class="header-logo logo" to="/"><img src="../assets/images/logo.png" alt="logo"></router-link>
+                <div class="header-controls">
+                    <button class="btn btn-primary">
+                        <img src="../assets/images/icons/user.png" alt="user">
+                        Войти
+                    </button>
+                    <button class="btn btn-outline" @click="openModal()">
+                        <img src="../assets/images/icons/shopping-cart.png" alt="cart">
+                        Корзина
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+    const emit = defineEmits(['toggleModal'])
+    const openModal = () => {
+        emit('toggleModal')
+    }
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    align-items: center;
+    min-height: 124px;
+}
+
+.header-box {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.header-controls {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 10px;
+}
+</style>
---
+++ src/views/ProductView.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <section class="banner">
+            <div class="container">
+                <div class="banner-item">
+                    <h2 class="banner-item--title">
+                        Онлайн-сервис<br>
+                        доставки еды на дом
+                    </h2>
+                    <p class="banner-item--subtitle">
+                        Блюда из любимого ресторана привезет курьер в перчатках, маске и с антисептиком
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="products">
+            <div class="container">
+                <div class="products-wrapper">
+                    <router-link class="products-card" v-for="rest in goodArray" :key="rest.id"
+                        :to="`/?id=${rest.id}`">
+                        <div class="products-card__image">
+                            <img :src="getUrl(rest.image)" alt="Sushi">
+                        </div>
+
+                        <div class="products-card__description">
+                            <div class="products-card__description-row">
+                                <h5 class="products-card__description--name">{{ rest.title }}</h5>
+                            </div>
+                            <div class="products-card__description-row">
+                                <p class="products-card__description--text">{{ rest.description }}</p>
+                            </div>
+                            <div class="products-card__description-row">
+                                <div class="products-card__description-controls">
+                                    <button class="btn btn-primary">
+                                        В корзину
+                                        <img src="../assets/images/icons/shopping-cart--white.svg" alt="cart">
+                                    </button>
+                                    <span class="products-card__description-controls--price">{{ rest.price }} ₽</span>
+                                </div>
+                            </div>
+                        </div>
+                    </router-link>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script setup>
+import { goodArray } from '../constants/rests'
+
+const getUrl = (name) => {
+    return new URL(`../assets/images/goods/${name}`, import.meta.url)
+}
+</script>
